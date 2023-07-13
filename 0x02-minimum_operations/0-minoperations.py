3e46d126bda97273107d8aa3b06f9ef91dd5beb2
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2023-07-13 23:53:41 +0100

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python3
 
-''' A module that returns the minimum Operations'''
+
+''' A module that returns the minimum Operations it takes to
+    get to n characters.
+
+    Available operations:
+        copy
+        paste
+'''
+
 
 def minOperations(n):
-     '''returns the minimum operations to get n H's'''
-     if (n < 2):
-        return 0
-    operations, root = 0, 2
-    while root <= n:
-        # if n evenly divides by root means no remainder
-        if n % root == 0:
-            # total even-divisions by root = total operations
-            operations += root
-            # set n to the remainder
-            n = n / root
-            # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
-        # increment root until it evenly-divides n
-        root += 1
-    return operations
+    '''
+    returns the minimum operations to get n H's
+    '''
+    min_operations = 0
+
+    if n <= 1:
+        return min_operations
+
+    for i in range(2, n + 1):
+        while n % i == 0:
+            min_operations += i
+            n /= i
+
+    return min_operations
+
+
+if __name__ == '__main__':
+    from random import randint
+    from time import time
+
+    start_time = time()
+
+    for i in range(10):
+        n = randint(2, 100)
+        print("Min # of operations to reach {} char: {}".
+              format(n, minOperations(n)))
+
+    print(f'==> Program completed in {time() - start_time:.3f}s')
