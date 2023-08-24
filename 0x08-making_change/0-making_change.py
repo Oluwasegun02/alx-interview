e6a66661542e12a110d8f21a318826eea11bb473
Oluwasegun02 <adebayoogunniyi8@gmail.com>
2023-08-24 17:41:57 +0100

    making_change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+ #!/usr/bin/python3
+
+ def makeChange(coins, total):
+
+     if total <= 0:
+         return 0
+     process_total = [float('inf')] * (total + 1)
+     process_total[0] = 0
+
+     for coin in coins:
+         for value in range(coin, total + 1):
+             process_total[value] = min(process[value], process_total[value - coin] + 1)
+
+         if process_total[total] == float('inf'):
+            return - 1
+        return process_total[total]
+
